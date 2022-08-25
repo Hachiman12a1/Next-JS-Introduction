1752aa453c1efd6f037ea9bc7a46cc19a5cb8370
Dao Van Dat <daovandat2k15@gmail.com>
2022-08-25 09:39:44 +0700

    323. Adding First Pages

--- pages/news.js
+++ pages/news.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 
-function HomePage(props) {
+function NewsPage(props) {
   return (
     <h1>
-      Home Page
+      News Page
     </h1>
   );
 }
 
-export default HomePage;
+export default NewsPage;
