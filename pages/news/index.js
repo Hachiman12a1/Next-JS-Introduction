3fcc3d21a04f822a8bbf32d1a1f741eb73ff2366
Dao Van Dat <daovandat2k15@gmail.com>
2022-08-25 11:18:42 +0700

    327. Linking Between Pages

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,11 +1,19 @@
-import React from 'react';
-
+import React, { Fragment } from "react";
+import Link from "next/link";
 
 function NewsPage(props) {
   return (
-    <h1>
-      News Page
-    </h1>
+    <Fragment>
+      <h1>News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-a-great-framework">
+            Next JS is a great framework
+          </Link>
+        </li>
+        <li>Something else</li>
+      </ul>
+    </Fragment>
   );
 }
 
